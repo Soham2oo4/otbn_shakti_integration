41b83a83ba670a57b137c27daba63264cf5fcd0c
Anmol Sahoo <anmol.sahoo25@gmail.com>
2018-11-27 11:25:24 +0530

    Updated the UART Flush function to C
    Removed the inline asm for UART flush and made it C

--- software/libfemto/drivers/shakti_e_platform.c
+++ software/libfemto/drivers/shakti_e_platform.c
@@ -9,13 +9,9 @@ static void shakti_e_init() {};
 static void shakti_e_poweroff(int status) {
 
     // UART Flush 
-    int8_t* uart_base_address = (int8_t*)0x1130c;
-    asm volatile("\n\t"
-      "uart_end:"
-      "li t1, 0x11300;" "\n\t"
-      "lb a1, 12(t1);" "\n\t"
-      "andi a1, a1, 0x1;" "\n\t"
-      "beqz a1, uart_end;" "\n\t");
+    static volatile int8_t* uart_base_address = (int8_t*)0x1130c;
+    while((*uart_base_address & 0x1) == 0);
+
     int *program_end_address = (int*) PROGRAM_END_REGISTER;
     *program_end_address = 1;
 };
