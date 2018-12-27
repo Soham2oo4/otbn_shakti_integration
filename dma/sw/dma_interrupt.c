690663d926165d56903233a6ae3ac3b5e292c0ed
Arjun Menon <c.arjunmenon@gmail.com>
2018-12-27 16:06:23 +0530

    Changed the source and destination from absolute addresses to  variables

--- dma/sw/dma_interrupt.c
+++ dma/sw/dma_interrupt.c
@@ -126,77 +126,71 @@ int main()
 	//int b[10];
 	//int c[15]={0xcafe0, 0xcafe1, 0xcafe2, 0xcafe3, 0xcafe4, 0xcafe5, 0xcafe6, 0xcafe7, 0xcafe8, 0xcafe9, 0xcafea, 0xcafeb, 0xcafec, 0xcafed, 0xcafee};
 	//int d[15];
+	int arr1[1000];
+	int arr2[1000];
     
-    int *a = (int*) 0x80003000; //Input from BRAM
-    int *c = (int*) 0x80004000; //Input from BRAM
+  int *a = arr1;//(int*) 0x80003000; //Input from BRAM
+  int *c = arr2;//(int*) 0x80004000; //Input from BRAM
 
-    int i;
-    int dum1_4byte=0x000000f0;
-    int dum2_4byte=0x000000d0;
-    int16_t dum1_2byte = 0x00f0;
-    int8_t dum1_byte = 0xf0;
+  int i;
+  int dum1_4byte=0x000000f0;
+  int dum2_4byte=0x000000d1;
+  int16_t dum1_2byte = 0x00f0;
+  int8_t dum1_byte = 0xf0;
 
-   for(i=0;i<10;++i){
-            *(a+i)= dum1_4byte+i;
-        //   *(c+i)= dum2_4byte+2*i;
-        }
+  printf("Address of a: %08x c: %08x",a,c);
+
+	//int burst=0;
+  for(int burst = 5; burst < 255; ++burst){
+
+  __asm__("fence\n\t");
+  	for(i=0;i<1000;++i) {
+    	*(a+i)= dum1_4byte+((burst+1)*i*2);
+      *(c+i)= dum2_4byte+((burst+2)*i);
+  }
+  __asm__("fence\n\t");
+	printf("a= %08x c= %08x\n burst= %d",*a,*c,burst);
  
-    __asm__("fence\n\t");
-        printf("Address of a: %08x c: %08x",a,c);
-
-	int burst=0;
-    //for(int burst = 0; burst < 256; ++burst){
-
-        
-	    *dma_cndtr3= 0xFA0;
-	    *dma_cmar3= a;
-	    *dma_cpar3= c;
-
-	    /**dma_cndtr5= 0xFA0;
-	    *dma_cmar5= c;
-	    *dma_cpar5= d;
-	    *dma_ccr5= 0x00086ADF; */
-	    //*dma_ccr3= 0x00076ADF; 
-        *dma_ccr3= (DMA_CCR_BURST_LEN(burst)|DMA_CCR_MEM2MEM|DMA_CCR_PL(2)|DMA_CCR_MSIZE(DMA_FOURBYTE)|DMA_CCR_PSIZE(DMA_FOURBYTE)|DMA_CCR_MINC|DMA_CCR_PINC|DMA_CCR_DIR|DMA_INTERRUPTS);
-        printf("\t DMA_CCR3 value: %08x\n",*dma_ccr3);
-	    //wait_for_dma_interrupt();
-			while(dma_flag==0) {
-				printf("DMA transaction ongoing...\n");
-				var1= read_csr(mip);
-				printf("mip: %08x\n",var1);
-				//waitfor(50);
-			}
-			printf("Transfer donee\n");
-        //printf("\t Clearing Interrupt Flags\n");
-	    
-        //Copying 1000 elements from A to B and C to D
-        for(i=0;i<1000;i++){
-            if(*(a+i)!=*(c+i)){
-                printf("\tDMA has gone wrong somewhere in copying a: %08x to c: %08x i:%d burst: %d\n",*(a+i),*(c+i),i,burst);
-                return -1;
-            }
-            //if(*(c+i)!=*(d+i)){
-            //    printf("\tDMA has gone wrong somewhere in copying c: %08x to d: %08x i:%d\n",*(c+i),*(d+i),i);
-            //    return -1;
-            //}
-        }
-
-        //a = (int*)0x80003000;
-        //b = (int*)0x20000;
-
-        //printf("\t Burst Value: %d seems to pass\n",burst);
-        //for(i=0;i<1000;++i){
-        //    *(b+i) = 0;
-        //}
-        //a = (int*)0x80003000;
-        //b = (int*)0x20000;
-
-
-    //}
-        printf("\t DMA copy from BRAM to TCM seems to work for all bursts\n");
-
-
-	    
     
-    return 0;
+	*dma_cndtr3= 0xFA0;
+	*dma_cmar3= a;
+	*dma_cpar3= c;
+	//*dma_ifcr=0xFFFFFFF;	//TODO this and the next statement gets optimised
+	//#pragma OPTIMIZE OFF
+	*dma_ifcr=0x0;
+	//#pragma OPTIMIZE ON
+
+	/**dma_cndtr5= 0xFA0;
+	*dma_cmar5= c;
+	*dma_cpar5= d;
+	*dma_ccr5= 0x00086ADF; */
+	//*dma_ccr3= 0x00076ADF; 
+    *dma_ccr3= (DMA_CCR_BURST_LEN(burst)|DMA_CCR_MEM2MEM|DMA_CCR_PL(2)|DMA_CCR_MSIZE(DMA_FOURBYTE)|DMA_CCR_PSIZE(DMA_FOURBYTE)|DMA_CCR_MINC|DMA_CCR_PINC|DMA_CCR_DIR|DMA_INTERRUPTS);
+    //printf("\t DMA_CCR3 value: %08x\n",*dma_ccr3);
+	//wait_for_dma_interrupt();
+	while(dma_flag==0) {
+		printf("DMA transaction ongoing...\n");
+		var1= read_csr(mip);
+		printf("mip: %08x\n",var1);
+		//waitfor(50);
+	}
+	printf("Transfer donee\n");
+	var1= read_csr(mip);
+	printf("mip: %08x\n",var1);
+	
+  for(i=0;i<1000;i++){
+  	if(*(a+i)!=*(c+i)){
+    	printf("\tDMA has gone wrong somewhere in copying a: %08x to c: %08x i:%d burst: %d\n",*(a+i),*(c+i),i,burst);
+      return -1;
+    }
+  }
+	printf("DMA works for burst length: %d\n",burst);
+	dma_flag=0;
+  }
+      printf("\t DMA copy from BRAM to TCM seems to work for all bursts\n");
+
+
+	  
+  
+  return 0;
 }
